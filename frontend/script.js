15bea6b13d68f7b38f29e9cf703d70cf11e8f62d
Joseph-Burks <joe_burks_3@yahoo.com>
2020-12-10 17:08:59 -0600

    its a start

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const addBreakfast = document.querySelector('#add-breakfast')
+    const searchContainer = document.querySelector('#search-container');
+    
+    addBreakfast.addEventListener('click', () => {
+    })
+
+})
