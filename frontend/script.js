8482da8db78342632912e915096a53e67c0982e3
Joseph-Burks <joe_burks_3@yahoo.com>
2020-12-11 10:37:21 -0600

    build search

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,35 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    const breakfastUL = document.querySelector('.breakfast-item-container');
+    const lunchUL = document.querySelector('.lunch-item-container');
+    const dinnerUL = document.querySelector('.dinner-item-container');
+    const snackUL = document.querySelector('.snack-item-container');
+
     const addBreakfast = document.querySelector('#add-breakfast')
+    const addLunch = document.querySelector('#add-lunch');
+    const addDinner = document.querySelector('#add-dinner');
+    const addSnack = document.querySelector('#add-snack');
+
+    const form = document.createElement('form')
+    const label = document.createElement('label')
+    label.innerText = 'Search foods: '
+    const input = document.createElement('input');
+    input.setAttribute('type', 'text')
+    const searchButton = document.createElement('button')
+    searchButton.innerText = 'Search'
+    form.append(label, input, searchButton)
+
     const searchContainer = document.querySelector('#search-container');
+    const buildSearch = () => {
+        searchContainer.innerHTML = ''
+        searchContainer.append(form)
+    }
     
     addBreakfast.addEventListener('click', () => {
+        let selectedFood = buildSearch()
     })
+    addLunch.addEventListener('click', () => {});
+    addDinner.addEventListener('click', () => {});
+    addSnack.addEventListener('click', () => {});
 
 })
